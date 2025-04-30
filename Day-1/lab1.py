99a203db0894a6c062a4757bf9ee171d883dac52
Desik@ <203578745+secopsarch@users.noreply.github.com>
2025-05-01 07:35:39 +0800

    day1-python

---
+++ Day-1/lab1.py
@@ -0,0 +1,31 @@
+# print("Hello, World!")
+# print ("this is my first python project")
+# print ("Hello, World!\nMy demo project")
+
+# String Concatenation
+# print("Hello" + "Desik@")
+
+# Take input from user using input function
+# # input("What is your name?")
+# print ("Hello " + input("What is your name?") + "!")
+
+# # Python Variables
+# value = input("What is your name?")
+# x = len(value)
+# print(x)
+
+# print(len(input("Hello, Welcome to python learning course")))
+
+# glass1 = "milk"
+# glass2 = "juice"
+
+# a = print(glass2)
+# b = print(glass1)
+
+# username = "demo"
+
+print("Welcome to Band Name Generator")
+a = input ("What's the name of the city you grew up in?\n")
+b = input ("What's your funtime activity?\n")
+
+print("your band name could be: " + a + " " + b) 
---
+++ Day-2/lab2.py
@@ -0,0 +1,7 @@
+# From the Lab yesterday
+print("welcome to lab2")
+name = input("what is your name?\n")
+age = input("what is your age?\n")
+value = print("Hello, " + name + " " "your age is: " + age)
+
+print("the length of name is:" + len(name))
