1c16320b9eee3f61f5d3f20898e78adaebeb4890
Desik@ <203578745+secopsarch@users.noreply.github.com>
2025-05-02 08:21:55 +0800

    day-2 labs

--- Day-2/lab2.py
+++ Day-2/lab2.py
@@ -1,7 +1,62 @@
-# From the Lab yesterday
-print("welcome to lab2")
-name = input("what is your name?\n")
-age = input("what is your age?\n")
-value = print("Hello, " + name + " " "your age is: " + age)
+# # # # From the Lab yesterday
+# # # print("welcome to lab2")
+# # # name = input("what is your name?\n")
+# # # age = input("what is your age?\n")
+# # # value = print("Hello, " + name + " " "your age is: " + age)
 
-print("the length of name is:" + len(name))
+# # print("HELLO"[3])
+
+# # print(True)
+# # print(False)
+
+# # print(len("Helloooo"))
+
+# # print(type("Arun"))
+# # print(type(2411342))
+# # print(type(123.01))
+# # print(type(True))
+
+# # print(int(123))
+
+# # name = input("Enter your name")
+# # len = len(name)
+# # # print(type(len))
+
+# # print("Number of letters in your name: " + str(len))
+
+# print(123 + 345)
+# print(3 * 3)
+# print(30/3)
+# print (10-1)
+# print(2**4)
+# print(23 // 2)
+
+# #PEMDASLR
+
+# print(3 * (3 + 3) / 3 - 3)
+
+# bmi = 63.31 / 2 ** 2
+# print(bmi)
+# print(int(bmi))
+# print(round(bmi, 2))
+
+# score = 0
+# score += 1
+# score = print(score)
+
+# score = 3
+# weight = 26.4
+# is_winning = True
+# print(f"Your Score is {score}, Your Weight is {weight}, you are winning this is {is_winning}")
+
+
+# print(round((2.7)))
+
+
+print("Welcome to the Tip Calculator")
+total_bill = int(input("what is the total bill? $"))
+tip = float(input("How much tip to give? 10, 15 or 20? "))
+number_of_person = int(input("How many people to split the bill? "))
+final_value = (total_bill + tip) / number_of_person
+amount_per_person = print(f"Each person should pay: ${final_value}")
+
