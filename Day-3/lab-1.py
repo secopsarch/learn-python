2fbbd93c4feb544856264ac31d5ba45394b8d350
Desik@ <203578745+secopsarch@users.noreply.github.com>
2025-05-03 15:30:50 +0800

    day3,lab2

--- Day-3/lab-1.py
+++ Day-3/lab-1.py
@@ -60,27 +60,44 @@
 # else:
 #     print("Thank you for choosing us!")
     
-print("Welcome to online pizza delivery")
-size = input("What size pizza do you want? S, M or L: ")
-topings = input("Do you want panner on your pizza? Y or N: ")
-add_on = input("Do you want Extra cheese? Y or N: ")
-bill = 0
+# print("Welcome to online pizza delivery")
+# size = input("What size pizza do you want? S, M or L: ")
+# topings = input("Do you want panner on your pizza? Y or N: ")
+# add_on = input("Do you want Extra cheese? Y or N: ")
+# bill = 0
 
-if size == "S":
-    bill += 15
-elif size == "M":
-    bill += 20
-elif size == "L":
-    bill += 25
+# if size == "S":
+#     bill += 15
+# elif size == "M":
+#     bill += 20
+# elif size == "L":
+#     bill += 25
+# else:
+#     print("please select from the available option")
+
+#     if topings == "Y":
+#         if size == "S":
+#             bill += 2
+#         else:
+#             bill += 3
+#     if add_on == "Y":
+#         bill += 1
+
+# print(f"your final bill is: ${bill}.")
+
+print("Welcome to the roller coaster")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("You can ride the rollercoaster!")
+    age = int(input("what is your age? "))
+    if age <= 7:
+        print("please pay 5$ ")
+    elif age <= 12:
+        print("please pay 12$ ")
+    elif age > 12 and age < 45 or age > 55:
+        print("please pay 20$ ")
+    elif age >= 45 or age <= 55:
+        print("no ticket for you, the admission is free!!!")
 else:
-    print("please select from the available option")
-
-    if topings == "Y":
-        if size == "S":
-            bill += 2
-        else:
-            bill += 3
-    if add_on == "Y":
-        bill += 1
-
-print(f"your final bill is: ${bill}.")
+    print("Sorry you cannot ride, you need to grow up!")
---
+++ Day-3/lab-2.py
@@ -0,0 +1,52 @@
+from random import choices
+from pyparsing import null_debug_action
+
+
+print('''
+            ___,
+       _.-'` __|__
+     .'  ,-:` \;',`'-,
+    /  .'-;_,;  ':-;_,'.
+   /  /;   '/    ,  _`.-\.
+  |  | '`. (`     /` ` \`|
+  |  |:.  `\`-.   \_   / |
+  |  |     (   `,  .`\ ;'|
+   \  \     | .'     `-'/
+    \  `.   ;/        .'
+     '._ `'-._____.-'`
+        `-.____|
+          _____|_____
+    abw  |___________|
+''')
+print("Welcome to the treasure Island")
+print("Your mission is to find the treasure")
+print("you're at a cross road. Where do you want to go?")
+side = (input('\t Type "left" or "right" \n')).lower()
+
+if side == "left":
+    print("You've come to a lake.")
+    action = (input('Type "wait" for the boat. Type "swim" to swim across\n')).lower()
+    if action == "wait":
+        print("There are three doors.")
+        color = (input(" One red, one yellow and one blue. Which color do you choose?\n")).lower()
+        if color == "blue":
+            print("Game Over, you choose blue!!")
+        elif color == "red":
+            print("Game Over, you choose red!!")
+        elif color == "yellow":
+            print("You Win...you choose yellow")
+        else:
+            print("not a valid color")
+    else:
+        print("you die")
+else:
+    print("You have fall into the hole, Game Over!.")
+
+
+        
+
+
+
+    
+
+
