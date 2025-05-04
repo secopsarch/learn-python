27a9864d06f247ccb1cf4edb85a7992ed999a996
Desik@ <203578745+secopsarch@users.noreply.github.com>
2025-05-04 11:25:56 +0800

    day-2 labs

---
+++ Day-4/lab-1.py
@@ -0,0 +1,26 @@
+import random
+import my_module
+
+random_number = my_module.my_number
+print(random_number)
+
+# random_number = random.randint(1, 10)
+# print(random_number)
+
+# random_float = random.random() * 10
+# print(random_float)
+
+# random_float0 = random.uniform(1, 10)
+# print(random_float0)
+
+# true = 1
+# false = 0
+
+# random_side = random.choice(['Head', 'Tails'])
+# print(random_side)
+
+random_side = random.randint(0, 1)
+if random_side == 1:
+    print("tails")
+else:
+    print("Heads")
