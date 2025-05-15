cccddb9213ee1c7ee4a00eb75b3947c10dd0f65e
Desik@ <203578745+secopsarch@users.noreply.github.com>
2025-05-15 17:14:40 +0800

    py labs-basic

---
+++ py-bible/1-lab/proj1.py
@@ -0,0 +1,6 @@
+##### Project one #### 
+
+import random from 
+
+health = 50
+
---
+++ py-bible/1-lab/variable.py
@@ -0,0 +1,19 @@
+# Python Examples and Learning
+
+# Variable declaration
+
+demo = True
+print(type(demo))
+demo = "Name"
+print(type(demo))
+
+test = 1+100
+print(test)
+hello = 3229+323
+print(hello + test)
+
+print(16 % 3)
+
+
+print (2 * (100-10))
+
